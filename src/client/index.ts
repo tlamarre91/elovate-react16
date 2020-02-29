514b1fb16d4e483047d3f3b508a2cd7a03138661
tom <tlamarre@gmail.com>
2020-02-28 18:40:27 -0600

    (temp?) fix for webpack error with dependency importing node built-ins

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,7 @@
-//import * as React from "react";
-//import { render } from "react-dom";
-//
-//import { log } from "./log";
-//import * as Api from "~shared/api";
-//
-//log.info("what it do");
-console.log("what it do");
+import * as React from "react";
+import { render } from "react-dom";
+
+import { log } from "./log";
+import * as Api from "~shared/api";
+
+log.info("what up dude");
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
     mode: process.env.NODE_ENV,
+    node: {
+        fs: "empty" // TODO: determine if this is an appropriate fix for webpack error with winston importing fs
+    },
     entry: "./src/client/index.ts",
     devtool: "inline-source-map",
     module: {
@@ -10,12 +13,16 @@ module.exports = {
             {
                 test: /\.ts$/,
                 use: "ts-loader",
-                exclude: /node_modules/
             }
         ]
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            "~shared": path.resolve("src/shared"),
+            "~server": path.resolve("src/server"),
+            "~client": path.resolve("src/client"),
+        }
     },
     output: {
         filename: "client.js",
